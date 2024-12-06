27faed504bad5f7980cc252984385cd3f7b41a6d
maf1ck <kosmanmatvij@gmail.com>
2024-12-06 20:03:19 +0200

    changes 2

--- js/task-3.js
+++ js/task-3.js
@@ -3,7 +3,7 @@ function getElementWidth(content,padding,border){
     const paddingWidth = parseFloat(padding) * 2; 
     const borderWidth = parseFloat(border) * 2;
     const sum = contentWidth + paddingWidth + borderWidth;
-    return;
+    return sum;
     
 }
 console.log(getElementWidth("50px", "8px", "4px"));
